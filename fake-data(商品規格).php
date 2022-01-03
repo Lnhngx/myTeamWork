b4877e790521ca797697b4a2124200c6faf02323
Alan <a8817388064@gmail.com>
2022-01-03 22:32:04 +0800

    商品欄位

--- fake-data(商品規格).php
+++ fake-data(商品規格).php
@@ -3,8 +3,7 @@
 require __DIR__ . '/parts/__connect_db.php';
 
 
-$sql = "INSERT INTO `商品規格`(`包裝長(cm)`, `包裝寬(cm)`, `包裝高(cm)`,`包裝重量(克)`
-    ) VALUES (?, ?, ?, ?)";
+$sql = "INSERT INTO `商品訊息`(`商品名稱`, `商品類型`, `商品規格`, `供應商`, `庫存訊息`,`商品價格`,`商品圖片`,`更新時間`) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
 //要塞值的地方習慣要塞問號,對應欄位,一個對一個
 
@@ -12,13 +11,17 @@ $pdo->beginTransaction();
 //會更快,過萬筆以上用這個會很迅速
 
 $stmt = $pdo->prepare($sql);
-for ($i = 0; $i < 1000; $i++) {
+for ($i = 0; $i < 20; $i++) {
 
     $stmt->execute([
-        rand(1,99),
-        rand(1,99),
-        rand(1,99),
-        rand(1,1000),
+        'test'.$i,
+        rand(1, 6),
+        rand(1000, 2000),
+        rand(1, 60),
+        rand(1, 60),
+        rand(250, 700),
+        rand(100000, 1640570263).'.jpg',
+        date("Y-m-d", rand(100000, 1640570263)),
     ]);
 }
 
