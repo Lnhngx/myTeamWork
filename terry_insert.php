1158163bee16e82a9fdf1c570cbdd505e5213f1c
Terry Liu <dassf2648@gmail.com>
2022-01-05 15:41:13 +0800

    移除include navbar

--- terry_insert.php
+++ terry_insert.php
@@ -9,7 +9,6 @@ $pageName = 'insert';
 
 ?>
 <?php include __DIR__ . '/parts/__html_head.php' ?>
-<?php include __DIR__ . '/parts/__navbar.php' ?>
 <div class="container">
     <div class="row">
         <div class="col-md-6">
