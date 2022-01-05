81f8372ed06eb734cf94429c49105ef154b129c2
Stan Lin <love4210kimo@gmail.com>
2022-01-05 23:11:22 +0800

    數量增減研究中

--- stan_temp_product.php
+++ stan_temp_product.php
@@ -9,9 +9,9 @@ $title = '購物車';
 <?php include __DIR__ . '/parts/__sidebar.php' ?>
 <?php
 
-$sql = sprintf('SELECT `product_sid`,`product_src`,`product`,`price`,`quantity`,`amount` FROM `temp_cart` WHERE 1');
+$sql = sprintf('SELECT `sid`,`product_name`,`src`,`price`,`quantity` FROM `temp_product` WHERE 1');
 $rows = $pdo->query($sql)->fetchAll();
-
+$number = 0;
 
 
 ?>
@@ -76,6 +76,13 @@ $rows = $pdo->query($sql)->fetchAll();
     .smallimg {
         width: 100%;
     }
+
+    #quantity {
+        box-sizing: border-box;
+        height: 35px;
+        width: 60px;
+        text-align: center;
+    }
 </style>
 <div class="wrap">
     <div class="container my-3">
@@ -102,19 +109,21 @@ $rows = $pdo->query($sql)->fetchAll();
                     <tbody>
                         <?php foreach ($rows as $r) : ?>
                             <tr class="tables">
-                                <th scope="row"><?= $r['product_sid'] ?></th>
-                                <td><?= $r['product_src'] ?>
-                                    <!-- <img class="smallimg" src="./pic/<?= $r['product_src'] ?>.png" ?> -->
+                                <?php $number++ ?>
+                                <th scope="row"><?= $number ?></th>
+                                <td style="display:none;"><?= $r['sid'] ?></td>
+                                <td><?= $r['product_name'] ?></td>
+                                <td><?= $r['src'] ?>
+                                    <!-- <img class="smallimg" src="./pic/<?= $r['src'] ?>.png" ?> -->
                                 </td>
-                                <td><?= $r['product'] ?></td>
                                 <td><?= $r['price'] ?></td>
-                                <td><?= $r['quantity'] ?></td>
-                                <td><?= $r['amount'] ?></td>
                                 <td>
-                                    <button type="button" class="editBtn btn btn-outline">修改</button>
-                                    <a href="javascript: removeCartItem(<?= $r['product_sid'] ?>)">
-                                        <button type="button" class="delBtn btn btn-outline">刪除</button>
-                                    </a>
+                                    <button class="btn btn-outline editBtn" onclick="btnOperate('-')">-</button>
+                                    <input id="quantity" value="<?= $r['quantity'] ?>">
+                                    <button class="btn btn-outline editBtn" onclick="btnOperate('+')">+</button>
+                                </td>
+                                <td>
+                                    <button type="button" class="editBtn btn btn-outline">加入購物車</button>
                                 </td>
                             </tr>
                         <?php endforeach;  ?>
@@ -126,12 +135,45 @@ $rows = $pdo->query($sql)->fetchAll();
 </div>
 
 <?php include __DIR__ . '/parts/__scripts.php' ?>
-
 <script>
-    function removeCartItem(product_sid) {
-        if (confirm(`確定要刪除編號為 ${product_sid} 的資料嗎?`)) {
-            location.href = `stan_delete_cart_api.php?product_sid=${product_sid}`;
+    function btnAdd() {
+        //1、獲取#quantity的value
+        let value = document.getElementById("quantity").value;
+        //2、將取出來的值做+1操作，再賦值給#quantity的value
+        value = Number(value) + 1;
+        document.getElementById("quantity").value = value;
+    }
+
+    function btnReduce() {
+        //1、獲取#quantity的值
+        let value = Number(document.getElementById("quantity").value);
+        //2、判斷#quantity的值是否小於等於1，如果小於等於1的話，則將值改為1
+        //3、否則，可以實現 - 1 操作
+        if (value <= 1) {
+            value = 1;
+        } else {
+            value -= 1;
         }
+        //4、將 value 的值賦值給 #quantity
+        document.getElementById("quantity").value = value;
+    }
+
+    /**
+     * 做 數值的更改操作
+     * 引數 op ：表示 符號 
+     */
+    function btnOperate(op) {
+        let value = Number($("quantity").value);
+        if (op == '+') {
+            value += 1;
+        } else if (op == '-') {
+            if (value <= 1) {
+                value = 1;
+            } else {
+                value -= 1;
+            }
+        }
+        $("quantity").value = value;
     }
 </script>
 <?php include __DIR__ . '/parts/__html_foot.php' ?>
