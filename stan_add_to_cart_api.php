a29db32e96493df12f55f30fcf4c33bea8f1f344
Stan Lin <love4210kimo@gmail.com>
2022-01-08 23:45:44 +0800

    新增至購物車API修改

--- stan_add_to_cart_api.php
+++ stan_add_to_cart_api.php
@@ -1,24 +1,21 @@
 <?php
 
-session_start();
-
 if (!isset($_SESSION['cart'])) {
     $_SESSION['cart'] = [];
 }
 
-
 $cardItem_sid = isset($_GET['sid']) ? intval($_GET['sid']) : 0;
 $cardItem_qty = isset($_GET['qty']) ? intval($_GET['qty']) : 0;
 
-if(! empty($sid)){
+if(! empty($cardItem_sid)){
     // TODO: 判斷有沒有那個商品
 
-    if(! empty($qty)){
-        // 新增或修改
-        $_SESSION['cart'][$sid] = $qty;
+    if(! empty($cardItem_qty)){
+        // 用來新增及修改
+        $_SESSION['cart'][$cardItem_sid] = $cardItem_qty;
     } else {
-        // 移除
-        unset($_SESSION['cart'][$sid]);
+        // 若此商品在購物車數量為0，就從購物車中移除
+        unset($_SESSION['cart'][$cardItem_sid]);
     }
 }
 header('Content-Type: application/json');
