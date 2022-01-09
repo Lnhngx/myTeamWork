4f1ad4c4d7326bcdfe0610e922aa9fc1f9c88f76
Stan Lin <love4210kimo@gmail.com>
2022-01-10 01:11:26 +0800

    購物車完成(大概)

--- stan_cart.php
+++ stan_cart.php
@@ -2,17 +2,35 @@
 
 require __DIR__ . '/parts/__connect_db.php';
 $pageName = 'Cart';
-$title = '購物車';
+$title = '奇怪的購物車';
+
+$addcartKeys = array_keys($_SESSION['cart']);
+// 抓購物車的產品SID
+
+$rows = [];
+$data_ar = [];
+
+if (!empty($addcartKeys)) {
+    $sql = sprintf("SELECT * FROM product_item  
+        where sid IN(%s)", implode(',', $addcartKeys));
+    $rows = $pdo->query($sql)->fetchAll();
+
+    foreach ($rows as $r) {
+        $r['quantity'] = $_SESSION['cart'][$r['sid']];
+        $data_ar[$r['sid']] = $r;
+    }
+}
 ?>
+
 
 <?php include __DIR__ . '/parts/__html_head.php' ?>
 
 <?php include __DIR__ . '/parts/__sidebar.php' ?>
 <?php
 
-$sql = sprintf('SELECT * FROM `temp_cart` WHERE 1');
-$rows = $pdo->query($sql)->fetchAll();
 $num = 0;
+$subtotal = 0;
+$total = 0;
 
 // 順序編號
 
@@ -104,12 +122,11 @@ $num = 0;
                     <button class="search btn btn-outline" type="submit">Search</button>
                 </form>
             </div>
-            <div class="bd-example my-5" onsubmit="sendData(); return false;">
+            <div class="bd-example my-5">
                 <table class="table table-hover">
                     <thead>
                         <tr>
                             <th scope="col">#</th>
-                            <th scope="col">產品</th>
                             <th scope="col">產品名稱</th>
                             <th scope="col">單價</th>
                             <th scope="col">數量</th>
@@ -118,33 +135,33 @@ $num = 0;
                         </tr>
                     </thead>
                     <tbody>
-                        <?php foreach ($rows as $r) : ?>
+                        <?php foreach ($_SESSION['cart'] as $sid => $qty) :
+                            $items = $data_ar[$sid]; ?>
                             <tr class="tables">
                                 <?php $num++ ?>
                                 <th scope="row"><?= $num ?></th>
-                                <td style="display:none;" data-sid="<?= $r['product_sid'] ?>"><?= $r['product_sid'] ?></td>
-                                <td><?= $r['product_src'] ?></td>
-                                <td><?= $r['product'] ?></td>
-                                <td class="price"><?= $r['price'] ?></td>
+                                <td class="sid" data-sid="<?= $items['sid'] ?>" style="display:none;"><?= $items['sid'] ?></td>
+                                <td><?= $items['name'] ?></td>
+                                <td class="price" data-price="<?= $items['price'] ?>"></td>
                                 <td>
-                                    <button class="btn btn-outline editBtn minusBtn">-</button>
-                                    <input class="quantitybox" value="<?= $r['quantity'] ?>">
-                                    <button class="btn btn-outline editBtn addBtn">+</button>
+                                    <button type="submit" class="btn btn-outline editBtn minusBtn">-</button>
+                                    <input type="text" class="quantitybox" data-qty="<?= $items['quantity'] ?>">
+                                    <button type="submit" class="btn btn-outline editBtn addBtn">+</button>
                                 </td>
-                                <td class="sub-total">
-                                </td>
+                                <td class="subtotal"></td>
                                 <td>
-                                    <a href="javascript: removeCartItem(<?= $r['product_sid'] ?>,<?= $num ?>)">
-                                        <button type="button" class="delBtn btn btn-outline">刪除</button>
+                                    <a href="javascript:removeCartItem(<?= $items['sid'] ?>,<?= $num ?> )">
+                                        <button type="button" class="delBtn btn btn-outline Joincart">刪除</button>
                                     </a>
                                 </td>
                             </tr>
                         <?php endforeach;  ?>
                     </tbody>
                 </table>
-                <a href="">
-                    <button type="button" class="orderBtn btn btn-outline">下一步</button>
-                </a>
+                <div id="total">
+                    總計：<?= $total ?>
+                </div>
+                <a href="stan_order.php" class="btn btn-success">結帳</a>
             </div>
         </div>
     </div>
@@ -153,23 +170,47 @@ $num = 0;
 <?php include __DIR__ . '/parts/__scripts.php' ?>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script>
-    function removeCartItem(product_sid, num) {
+    function removeCartItem(sid, num) {
         if (confirm(`確定要刪除編號 ${num} 的資料嗎?`)) {
-            location.href = `stan_delete_cart_api.php?product_sid=${product_sid}`;
+            location.href = `stan_delete_cart_api.php?sid=${sid}`;
         }
     }
+
+    function priceCommas(n) {
+        return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+    };
+
 
     document.querySelectorAll('.minusBtn').forEach((el) => {
         el.addEventListener('click', btnminus);
     })
 
     function btnminus(event) {
-        let input = event.currentTarget.nextElementSibling;
-        if (input.value <= 1) {
-            input.value = 1;
+        // let input = event.currentTarget.nextElementSibling;
+
+        // if (input.value <= 1) {
+        //     input.value = 1;
+        // } else {
+        //     input.value -= 1;
+        // }
+
+        let sid = $(this).closest('.tables').find('.sid').text();
+        let qty = $(this).closest('.tables').find('.quantitybox').val();
+
+        if (qty <= 1) {
+            qty = 1;
         } else {
-            input.value -= 1;
+            qty -= 1;
         }
+
+        $.get('stan_add_to_cart_api.php', {
+            sid,
+            qty
+        }, function() {
+            calPrices(function() {
+                location.reload();
+            });
+        }, 'json');
     }
 
     document.querySelectorAll('.addBtn').forEach((el) => {
@@ -177,17 +218,48 @@ $num = 0;
     })
 
     function btnadd(event) {
-        let input = event.currentTarget.previousElementSibling;
-        // let a = document.querySelector('.quantitybox').parentNode;
-        //     a =a.previousElementSibling;
-        input.value = parseInt(input.value) + 1;
-        changeQty(input.value);
-        // 直接寫 input.value+=1; 系統會判斷成字串相加，故需使用 parseInt 轉換後在相加
-    }
+        // let input = event.currentTarget.previousElementSibling;
+        // input.value = parseInt(input.value) + 1;
+        // // 直接寫 input.value+=1; 系統會判斷成字串相加，故需使用 parseInt 轉換後在相加
 
-    function changeQty(val) {
-        let qty = val;
-        console.log(qty);
+        let sid = $(this).closest('.tables').find('.sid').text();
+        let qty = $(this).closest('.tables').find('.quantitybox').val();
+        qty = parseInt(qty) + 1;
+
+        $.get('stan_add_to_cart_api.php', {
+            sid,
+            qty
+        }, function() {
+            calPrices(function() {
+                location.reload();
+            });
+        }, 'json');
+    };
+
+    function calPrices(cb) {
+        let total = 0;
+        const table = $('.tables');
+        if (! table.length) {
+            alert('請先將商品加入購物車');
+            location.href = 'stan_temp_product.php';
+            return;
+        }
+
+        table.each(function(i, el) {
+            let price = $(this).closest('.tables').find('.price');
+            price.text('$' + price.attr('data-price'));
+
+            let qty = $(this).closest('.tables').find('.quantitybox');
+            qty.val(qty.attr('data-qty'));
+
+            let subtotal = $(this).closest('.tables').find('.subtotal');
+            subtotal.text('$' + priceCommas(price.attr('data-price') * qty.val()));
+
+            total += price.attr('data-price') * qty.val();
+        });
+        $('#total').text('$' + priceCommas(total));
+        if (cb) cb();
     }
+    calPrices();
 </script>
 <?php include __DIR__ . '/parts/__html_foot.php' ?>
