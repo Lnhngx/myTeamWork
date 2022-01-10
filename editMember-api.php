22ab3e98bb8fbc94f2216f3eb805d77c6ff2b52e
TingYun <tingyun02@hotmail.com>
2022-01-10 15:07:00 +0800

    等級checked

--- editMember-api.php
+++ editMember-api.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__.'/parts/__connect_db.php';
+require __DIR__ . '/parts/__connect_db.php';
 
 $output = [
     'success' => false,
@@ -7,14 +7,14 @@ $output = [
     'error' => '',
 ];
 
-if(! isset($_SESSION['users'])){
+if (!isset($_SESSION['users'])) {
     header("Location: member_login.php");
     exit;
 }
 // 沒有登入就轉向
 
 $sid = isset($_POST['sid']) ? intval($_POST['sid']) : 0;
-if(empty($sid)){
+if (empty($sid)) {
     $output['code'] = 400;
     $output['error'] = '沒有此會員';
     echo json_encode($output, JSON_UNESCAPED_UNICODE);
@@ -27,25 +27,25 @@ $mobile = $_POST['mobile'] ?? '';
 $password = $_POST['password'] ?? '';
 
 
-if(empty($name)){
+if (empty($name)) {
     $output['code'] = 403;
     $output['error'] = '請輸入正確的姓名';
     echo json_encode($output, JSON_UNESCAPED_UNICODE);
     exit;
 }
-if(empty($email) or !filter_var($email, FILTER_VALIDATE_EMAIL)){
+if (empty($email) or !filter_var($email, FILTER_VALIDATE_EMAIL)) {
     $output['code'] = 401;
     $output['error'] = '請輸入正確的email';
     echo json_encode($output, JSON_UNESCAPED_UNICODE);
     exit;
 }
-if(empty($mobile) or !preg_match("/^09\d{2}-?\d{3}-?\d{3}$/", $mobile)){
+if (empty($mobile) or !preg_match("/^09\d{2}-?\d{3}-?\d{3}$/", $mobile)) {
     $output['code'] = 405;
     $output['error'] = '請輸入正確的手機號碼';
     echo json_encode($output, JSON_UNESCAPED_UNICODE);
     exit;
 }
-if(empty($password)){
+if (empty($password)) {
     $output['code'] = 407;
     $output['error'] = '請輸入密碼';
     echo json_encode($output, JSON_UNESCAPED_UNICODE);
@@ -58,7 +58,8 @@ $sql = "UPDATE `members` SET
                      `password`=?,
                      `mobile`=?,
                      `birthday`=?,
-                     `address`=?
+                     `address`=?,
+                     `grade_sid`=?,
         WHERE `sid`=?";
 
 $stmt = $pdo->prepare($sql);
@@ -69,6 +70,7 @@ $stmt->execute([
     $mobile,
     empty($_POST['birthday']) ? NULL : $_POST['birthday'],
     $_POST['address'] ?? '',
+    $_POST['grade_sid'] ?? '',
     $sid
 ]);
 
@@ -77,9 +79,9 @@ $stmt->execute([
 
 
 
-if($stmt->rowCount()==0){
+if ($stmt->rowCount() == 0) {
     $output['error'] = '資料沒有修改';
-}else{
+} else {
     $output['success'] = true;
 }
 
--- editMember.php
+++ editMember.php
@@ -2,7 +2,7 @@
 
 require __DIR__ . '/parts/__connect_db.php';
 $title = '修改資料';
-if (! isset($_GET['sid'])) {
+if (!isset($_GET['sid'])) {
     header("Location: memberList.php");
     exit;
 }
@@ -19,105 +19,108 @@ if (empty($row)) {
 <?php include __DIR__ . '/parts/__html_head.php' ?>
 <?php include __DIR__ . '/parts/__sidebar.php' ?>
 <style>
-    .container {
-        position: absolute;
-        right: 0;
+    .wrap {
         width: calc(100% - 250px);
-        margin-top: 20px;
-        margin-bottom: 20px;
+        position: absolute;
+        left: 250px;
+        /* text-align: center; */
     }
-    .row {
-        justify-content: center;
-    }
+
     .subbtn {
         background-color: #2f4f4f;
         border-color: #2f4f4f;
     }
+
     .subbtn:hover {
         background-color: #908a70;
         border-color: #908a70;
     }
+
+    .form-text {
+        color: crimson;
+    }
 </style>
-<div class="container">
-    <div class="row">
-        <div class="col-md-6">
-            <div class="card">
-                <div class="card-body">
-                    <h3 class="card-title text-center">修改會員基本資料</h3>
+<div class="wrap">
+    <div class="container">
+        <div class="row">
+            <div class="col-6 mx-auto mt-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h3 class="card-title text-center">修改會員基本資料</h3>
 
-                    <form name="form_member" onsubmit="sendData(); return false;">
-                        <input type="hidden" name="sid" value="<?= $row['sid'] ?>">
-                        <div class="mb-3">
-                            <label for="email" class="form-label">Account (Email)</label>
-                            <input type="text" class="form-control" id="email" name="email" value="<?= $row['email'] ?>">
-                            <div class="form-text"></div>
-                        </div>
-                        <div class="mb-3">
-                            <label for="name" class="form-label">Name</label>
-                            <input type="text" class="form-control" id="name" name="name" value="<?= htmlentities($row['name']) ?>">
-                            <div class="form-text"></div>
-                            <!-- 將所有適用的字符轉換為 HTML 實體 -->
-                        </div>
-                        <div class="mb-3">
-                            <label for="password" class="form-label">Password</label>
-                            <input type="text" class="form-control" id="password" name="password" value="<?= htmlentities($row['password']) ?>">
-                            <div class="form-text"></div>
-                        </div>
-                        <div class="mb-3">
-                            <label for="mobile" class="form-label">Mobile</label>
-                            <input type="text" class="form-control" id="mobile" name="mobile" data-pattern="09\d{2}-?\d{3}-?\d{3}" value="<?= $row['mobile'] ?>">
-                            <div class="form-text"></div>
-                        </div>
-                        <div class="mb-3">
-                            <label for="birthday" class="form-label">Birthday</label>
-                            <input type="date" class="form-control" id="birthday" name="birthday" value="<?= $row['birthday'] ?>">
-                            <div class="form-text"></div>
-                        </div>
-                        <div class="mb-3">
-                            <label for="address" class="form-label">Address</label>
-                            <textarea name="address" id="address" cols="30" rows="3"><?= htmlentities($row['address']) ?></textarea>
-                            <div class="form-text"></div>
-                        </div>
-                        <?php /*
-                        <div class="mb-3">
-                            <label for="grade_sid" class="form-label">Grade</label>
-                            <br>
-                            <input type="radio" name="grade_sid" value="<?= $row['grade_sid'] ?>">一般
-                            <input type="radio" name="grade_sid" value="<?= $row['grade_sid'] ?>">黃金
-                            <input type="radio" name="grade_sid" value="<?= $row['grade_sid'] ?>">白金
-                            <input type="radio" name="grade_sid" value="<?= $row['grade_sid'] ?>">鑽石
-                            <div class="form-text"></div>
-                        </div>
-                        */ ?>
+                        <form name="form_member" onsubmit="sendData(); return false;">
+                            <input type="hidden" name="sid" value="<?= $row['sid'] ?>">
+                            <div class="mb-3">
+                                <label for="email" class="form-label">Account (Email)</label>
+                                <input type="text" class="form-control" id="email" name="email" value="<?= $row['email'] ?>">
+                                <div class="form-text"></div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="name" class="form-label">Name</label>
+                                <input type="text" class="form-control" id="name" name="name" value="<?= htmlentities($row['name']) ?>">
+                                <div class="form-text"></div>
+                                <!-- 將所有適用的字符轉換為 HTML 實體 -->
+                            </div>
+                            <div class="mb-3">
+                                <label for="password" class="form-label">Password</label>
+                                <input type="text" class="form-control" id="password" name="password" value="<?= htmlentities($row['password']) ?>">
+                                <div class="form-text"></div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="mobile" class="form-label">Mobile</label>
+                                <input type="text" class="form-control" id="mobile" name="mobile" data-pattern="09\d{2}-?\d{3}-?\d{3}" value="<?= $row['mobile'] ?>">
+                                <div class="form-text"></div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="birthday" class="form-label">Birthday</label>
+                                <input type="date" class="form-control" id="birthday" name="birthday" value="<?= $row['birthday'] ?>">
+                                <div class="form-text"></div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="address" class="form-label">Address</label>
+                                <textarea name="address" id="address" cols="30" rows="3"><?= htmlentities($row['address']) ?></textarea>
+                                <div class="form-text"></div>
+                            </div>
 
-                        <button type="submit" class="btn btn-primary">修改</button>
-                    </form>
+                            <div class="mb-3">
+                                <label for="grade_sid" class="form-label">Grade</label>
+                                <br>
+                                <input type="radio" name="grade_sid" value="1" <?php if($row['grade_sid']=='1'){echo ("checked");} ?>>一般
+                                <input type="radio" name="grade_sid" value="2" <?php if($row['grade_sid']=='2'){echo ("checked");} ?>>黃金
+                                <input type="radio" name="grade_sid" value="3" <?php if($row['grade_sid']=='3'){echo ("checked");} ?>>白金
+                                <input type="radio" name="grade_sid" value="4" <?php if($row['grade_sid']=='4'){echo ("checked");} ?>>鑽石
+                                <div class="form-text"></div>
+                            </div>
 
+
+                            <button type="submit" class="btn btn-primary">修改</button>
+                        </form>
+
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">資料錯誤</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    ...
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">資料錯誤</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
+    </div>
 </div>
-
 
 
 
