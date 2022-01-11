ae5d7f1b78b44a5a077d5aab4580df7418d674ec
tony-hsueh <yahooleo36@gmail.com>
2022-01-11 11:07:30 +0800

    final change

--- parts/__sidebar.php
+++ parts/__sidebar.php
@@ -176,7 +176,7 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a href="#" class="nav-link text-white " aria-current="page">
+                <a href="jerry_index_.php" class="nav-link text-white " aria-current="page">
                     <div class="icon"><i class="fas fa-paw"></i></div>
                     <p>園區導覽</p>
 
@@ -200,7 +200,7 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a href="#" class="nav-link text-white " aria-current="page">
+                <a href="gameList.php" class="nav-link text-white " aria-current="page">
                     <div class="icon"><i class="fas fa-puzzle-piece">
 
                         </i></div>
