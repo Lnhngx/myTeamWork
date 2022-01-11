a3b92631e72e53994b254b219146a9d9c94734cf
TingYun <tingyun02@hotmail.com>
2022-01-11 17:03:43 +0800

    搜尋

--- memberList.php
+++ memberList.php
@@ -5,6 +5,8 @@ $pageName = 'index';
 $title = '會員資料列表';
 $pageName = 'memberList';
 
+$keyword = isset($_GET['keyword']) ? $_GET['keyword'] : '';
+// 搜尋欄位
 if (!isset($_SESSION['users'])) {
     header("Location: login.php");
     exit;
@@ -18,8 +20,13 @@ if ($page < 1) {
     exit;
 }
 // 頁數不能為負數
-$t_sql = "SELECT COUNT(1) FROM members";
+$t_sql = "SELECT COUNT(1) FROM `members` JOIN `grade` ON `members`.`grade_sid`=`grade`.`grade_sid`";
 // 資料筆數
+// SELECT * FROM `members` LEFT JOIN `grade` ON `grade`.`grade_sid`=`members`.`grade_sid`
+if (isset($_GET['keyword'])) {
+    $t_sql = $t_sql . "WHERE email LIKE '%" . $keyword . "%' ";
+}
+// 如果有關鍵字
 $totalRows = $pdo->query($t_sql)->fetch(PDO::FETCH_NUM)[0];
 $totalPages = ceil($totalRows / $perpage);
 if ($page > $totalPages) {
@@ -28,22 +35,24 @@ if ($page > $totalPages) {
 }
 
 // $sql = sprintf("SELECT * FROM members ORDER BY sid DESC LIMIT %s, %s", ($page - 1) * $perpage, $perpage);
-$sql = sprintf("SELECT * 
-                    FROM `members` m
-                    LEFT JOIN `grade` g
-                    ON m.`grade_sid`=g.`grade_sid`
-                    ORDER BY m.`sid` DESC 
-                    LIMIT %s, %s", ($page - 1) * $perpage, $perpage);
-$rows = $pdo->query($sql)->fetchAll();
+// $sql = sprintf("SELECT * 
+//                     FROM `members` m
+//                     LEFT JOIN `grade` g
+//                     ON m.`grade_sid`=g.`grade_sid`
+//                     ORDER BY m.`sid` DESC 
+//                     LIMIT %s, %s", ($page - 1) * $perpage, $perpage);
+$sqlsch = isset($_GET['keyword']) ? "WHERE `email` LIKE '%" . $keyword . "%'" : '';
+// SELECT * FROM `members` LEFT JOIN `grade` ON `grade`.`grade_sid`=`members`.`grade_sid` WHERE `mobile` LIKE '%4%' ORDER BY `sid` DESC LIMIT 5,5
+$sql = sprintf("SELECT * FROM `members` LEFT JOIN `grade` ON `grade`.`grade_sid`=`members`.`grade_sid` %s ORDER BY `sid` DESC LIMIT %s,%s", $sqlsch, ($page - 1) * $perpage, $perpage);
 
+$rows = $pdo->query($sql)->fetchAll();
 
 
 ?>
 
 <?php include __DIR__ . '/parts/__html_head.php' ?>
 <?php include __DIR__ . '/parts/__sidebar.php' ?>
-<script src="/myTeamWork/search.js"></script>
-<!-- search bar 語法-->
+
 <style>
     .fa-angle-double-right,
     .fa-angle-right,
@@ -92,7 +101,6 @@ $rows = $pdo->query($sql)->fetchAll();
     .search,
     .insert,
     .editBtn {
-        text-align: left;
         background-color: #2f4f4f;
         color: white
     }
@@ -110,18 +118,18 @@ $rows = $pdo->query($sql)->fetchAll();
     }
 
     .editBtn,
-    .delBtn,
-    .delAllbtn {
+    .delAllBtn,
+    .delBtn {
         color: white;
     }
 
     .delBtn,
-    .delAllbtn {
+    .delAllBtn {
         background-color: #C82C2C;
     }
 
-    .delBtn:hover,
-    .delAllbtn:hover {
+    .delAllBtn:hover,
+    .delBtn:hover {
         background-color: #9A572D;
         color: white;
     }
@@ -131,16 +139,25 @@ $rows = $pdo->query($sql)->fetchAll();
         /* text-align: center; */
         vertical-align: middle;
     }
+
+    .myimg {
+        width: 100px;
+    }
+
+    .introduction {
+        width: 250px;
+    }
 </style>
 <div class="wrap">
     <div class="container my-3">
         <div class="row">
             <div class="col-3 d-flex" style="justify-content: flex-start;"><button onclick="insertMember()" type="button" class="insert btn btn-outline" id="btn">新增</button></div>
-            <div class="col-3 d-flex" style="justify-content: flex-end;">
-                <div class="d-flex">
-                    <input class="searchIp form-control light-table-filter" type="search" placeholder="Search" aria-label="Search" data-table="order-table">
-                    <!-- search bar 語法-->
-                </div>
+            <div class="col-3 d-flex" style="justify-content:flex-end;">
+                <form class="d-flex">
+                    <input id="searchIp" class="searchIp light-table-filter" type="search" placeholder="請輸入搜尋關鍵字" aria-label="Search">
+
+                    <button class="searchIpButton search btn btn-outline" type="button">Search</button>
+                </form>
             </div>
             <div class="col-12">
                 <div class="col">
@@ -166,7 +183,7 @@ $rows = $pdo->query($sql)->fetchAll();
                             <?php endif; ?>
                             <!-- 頁數 -->
                             <li class="page-item <?= $totalPages == $page ? 'disabled' : '' ?>">
-                                <a class="page-link" href="?page=<?= $page + 1 ?>">
+                                <a class="page-link" href="<?= isset($_GET['keyword'])?"?keyword=$keyword&page=$i":"?page=$i"?>">
                                     <i class="fas fa-angle-right"></i>
                                 </a>
                             </li>
@@ -186,7 +203,7 @@ $rows = $pdo->query($sql)->fetchAll();
             <div class="bd-example mb-5 mt-3">
                 <form action="deleteAll_member-api.php" method="post" name="form1">
                     <!-- 表單送出會到 deleteAll_member-api -->
-                    <table class="table table-hover order-table">
+                    <table class="table table-hover">
                         <!-- search bar 語法-->
                         <thead>
                             <tr>
@@ -195,18 +212,18 @@ $rows = $pdo->query($sql)->fetchAll();
                                     <label for="ckbAll">全選</label>
                                 </th>
                                 <!-- 勾選 -->
-                                <th scope="col">Member_ID</th>
-                                <th scope="col">Account (Email)</th>
-                                <th scope="col">Name</th>
-                                <th scope="col">Password</th>
-                                <th scope="col">Mobile</th>
-                                <th scope="col">Birthday</th>
-                                <th scope="col">Address</th>
-                                <th scope="col">Grade</th>
-                                
+                                <th scope="col">#</th>
+                                <th scope="col">電子郵件</th>
+                                <th scope="col">姓名</th>
+                                <th scope="col">密碼</th>
+                                <th scope="col">手機</th>
+                                <th scope="col">生日</th>
+                                <th scope="col">地址</th>
+                                <th scope="col">等級</th>
+
                                 <th scope="col">
                                     <button type="submit" class="delAllbtn btn btn-outline-dange">
-                                        <i class="fas fa-trash-alt"></i>
+                                        勾選 <i class="fas fa-trash-alt"></i>
                                     </button>
                                 </th>
                             </tr>
@@ -226,9 +243,9 @@ $rows = $pdo->query($sql)->fetchAll();
                                     <td><?= $r['birthday'] ?></td>
                                     <td><?= $r['address'] ?></td>
                                     <td><?= $r['grade_name'] ?></td>
-                                    
+
                                     <td>
-                                        <a href="editMember.php?sid=<?= $r['sid'] ?>">
+                                        <a href="editMember.php?sid=<?= $r['sid'] ?>" style="text-decoration: none;">
                                             <button type="button" class="editBtn btn btn-outline">修改</button>
                                         </a>
                                         <!-- 修改 -->
@@ -278,7 +295,7 @@ $rows = $pdo->query($sql)->fetchAll();
     })
     // 如果第一個checkbox已選，其他全選
 
-    
+
     const delAllbtn = document.querySelector('.delAllbtn');
     delAllbtn.addEventListener('click', delAll);
 
@@ -291,12 +308,26 @@ $rows = $pdo->query($sql)->fetchAll();
             body: fd,
         }).then(r => r.json()).then(obj => {
             if (obj.success) {
-                if(confirm(`確定刪除已勾選的項目?`)){
+                if (confirm(`確定刪除已勾選的項目?`)) {
                     window.location.reload();
                 }
             }
         });
-        
+
     }
+
+
+    const searchIp = document.querySelector('#searchIp');
+    const searchIpButton = document.querySelector('.searchIpButton');
+    let str = '';
+    
+
+    function searchTest(v) {
+        event.preventDefault();
+        const searchIpValue = searchIp.value;
+        str = searchIpValue;
+        window.location.href = "http://localhost/myTeamWork/memberList.php?keyword=" + str;
+    }
+    searchIpButton.addEventListener('click', searchTest);
 </script>
 <?php include __DIR__ . '/parts/__html_foot.php' ?>
